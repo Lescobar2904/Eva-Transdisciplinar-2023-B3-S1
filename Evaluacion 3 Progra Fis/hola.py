9b91f1de4c5fce2ab3d33170cfb91eec440625f3
JoseCalfiman <jcalfiman2023@alu.uct.cl>
2023-05-30 11:32:10 -0400

    Agregado de imagenes al fondo y botones

--- Evaluacion 3 Progra Fis/hola.py
+++ Evaluacion 3 Progra Fis/hola.py
@@ -7,11 +7,15 @@ pg.display.set_mode(nRES)
 
 ventana  =  tk.Tk()
 ventana.geometry("1000x700")
+ima_emp = tk.PhotoImage(file="start.png")
+ima_emp2 = tk.PhotoImage(file="Formula.png")
+ima_emp3 = tk.PhotoImage(file="Valores.png")
+ima_emp4 = tk.PhotoImage(file="fpulentox.png")
 
-fondore = tk.Label(ventana, text="", bg= "sky blue")
+fondore = tk.Label(ventana, image=ima_emp4)
 fondore.place(x=0, y=0, width=1000, height=700)
 
-boton1= tk.Button(ventana, text = "Empieza", bg = "green")
+boton1= tk.Button(ventana, image=ima_emp)
 boton2= tk.Button(ventana, text = "Forma 1", bg = "yellow")
 boton3= tk.Button(ventana, text = "Forma 2", bg = "yellow")
 
@@ -24,26 +28,18 @@ li.place(x=372, y=0, width=15, height=700)
 li2 = tk.Label(ventana, text="", bg= "black")
 li2.place(x=387, y=461, width=614, height=15)
 
-f = tk.Label(ventana, text="", bg= "orange")
+
+f = tk.Label(ventana, image=ima_emp3)
 f.place(x=0, y=430, width=372, height=178)
-ecua = tk.Label(ventana, text="EPG= m * g * h", bg= "orange")
+ecua = tk.Label(ventana, image=ima_emp2)
 ecua.place(x=0, y=0, width=203, height=99)
 
-fondo=tk.Label(ventana, text= "Eliga sus valores (m en kg) y (h en metros)", bg= "red")
-fondo.place(x=0, y=390, width=372, height=50)
-m=tk.Label(ventana, text= "m= ", bg= "orange")
-g=tk.Label(ventana, text= "g= ", bg= "orange")
-h=tk.Label(ventana, text= "h= ", bg= "orange")
-m.place(x=0, y=490, width=60, height=30)
-h.place(x=0, y=520, width=60, height=30)
-g.place(x=0, y=550, width=60, height=30)
-
 m2 = tk.Entry(ventana, bg= "pink")
-m2.place(x=50, y=495, width=50, height=20)
+m2.place(x=50, y=525, width=50, height=20)
 h2 = tk.Entry(ventana, bg= "pink")
-h2.place(x=50, y=525, width=50, height=20)
+h2.place(x=50, y=550, width=50, height=20)
 g2 = tk.Entry(ventana, bg= "pink")
-g2.place(x=50, y=555, width=50, height=20)
+g2.place(x=50, y=575, width=50, height=20)
 
 
 
