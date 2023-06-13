70ef40a5c8eec0a4d31d07f9e14928d51cd6f4a5
saezjose <134319766+saezjose@users.noreply.github.com>
2023-06-13 11:05:20 -0400

    Update prueba_grafico.py
    esta es una demo del grafico a usar en la simulacion

--- Evaluacion 3 Progra Fis/prueba_grafico.py
+++ Evaluacion 3 Progra Fis/prueba_grafico.py
@@ -1,11 +1,8 @@
-import telepot
-from telepot.loop import MessageLoop
+import matplotlib.pyplot as plt
 
-bot = telepot.Bot('6011808035:AAGCtR8laNtCm5P185t4Ez3o2CWYAmGWUd0')
+plt.plot([4, 3, 2, 1], [16, 9, 4, 1])
 
-bot.getMe()
+plt.ylabel('y numbers')
+plt.xlabel('x numbers')
 
-bot.sendMessage(6182402771, 'Hola!')
-bot.sendMessage(6182402771, 'Soy Purah')
-bot.sendMessage(6182402771, 'Soy un Bot creado para hacer uso del trabajo')
-bot.sendMessage(6182402771, 'numero 3 de programacion Robot de la Universidad Catolica de temuco')
+plt.show()
