59c6446c5b095c482a4fa2a8a0846332ce087f79
saezjose <134319766+saezjose@users.noreply.github.com>
2023-06-13 11:00:27 -0400

    Add files via upload
    esta es una demo del grafico de la simulacion

---
+++ Evaluacion 3 Progra Fis/prueba_grafico.py
@@ -0,0 +1,11 @@
+import telepot
+from telepot.loop import MessageLoop
+
+bot = telepot.Bot('6011808035:AAGCtR8laNtCm5P185t4Ez3o2CWYAmGWUd0')
+
+bot.getMe()
+
+bot.sendMessage(6182402771, 'Hola!')
+bot.sendMessage(6182402771, 'Soy Purah')
+bot.sendMessage(6182402771, 'Soy un Bot creado para hacer uso del trabajo')
+bot.sendMessage(6182402771, 'numero 3 de programacion Robot de la Universidad Catolica de temuco')
